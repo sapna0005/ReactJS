d675b856e20aaf1084d9d6be848e8c409f992e90
sapna0005 <sapna.sahani0005@gmail.com>
2025-04-03 11:43:27 +0530

    added react fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 
 const App=()=>{
   return(
+    <>
     <h1>Welcome to React class!!!!</h1>
+    <p>Welcome to Cybrom</p>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, labore odio. Accusantium magnam nam quo perspiciatis, debitis eum sequi officiis atque animi, error quas, aliquam rem a eveniet aperiam molestiae.</h2>
+    </>
   )
 }
 export default App;
