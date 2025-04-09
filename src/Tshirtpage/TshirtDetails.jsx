88791922407decaa2ce4cd922d94ca946b768e94
sapna0005 <sapna.sahani0005@gmail.com>
2025-04-09 06:13:46 +0530

    added project

--- src/Tshirtpage/TshirtDetails.jsx
+++ src/Tshirtpage/TshirtDetails.jsx
@@ -1,12 +1,21 @@
-
+import s1 from "./tshirt2.1.jpg"
+import s2 from "./tshirt2.2.jpg"
+import s3 from "./tshirt2.3.jpg"
+import s4 from "./tshirt2.4.jpg"
 
 const TshirtDetails=()=>{
     return(
         <>
-         <div style={{display:"flex", flexDirection:"row"}}>
+         <div style={{display:"flex", flexDirection:"row"}} >
            <div className="tshirtpage">
-
+            
+            <img src={s1} style={{height:"350px", width:"350px"}}/>
+            <img src={s2} style={{height:"350px", width:"350px"}}/>
+            <img src={s3} style={{height:"350px", width:"350px"}}/>
+            <img src={s4} style={{height:"350px", width:"350px"}}/>
            </div>
+          
+          
 
          </div>
         </>
